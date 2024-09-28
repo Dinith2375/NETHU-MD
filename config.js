a4f62c36c14084b898334eca86dcd3e7d72fc579
Dinith2375 <137248628+Dinith2375@users.noreply.github.com>
2024-09-28 20:07:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IvcgxZAa#C7wAUz6FPdqhk5bCUZkDYm8ZfNw6EuLcjkDnpk3UWe4",
+SESSION_ID: process.env.SESSION_ID || "1XNTTKZR#m7Vqdi2B8ZF3Lh0W2jAuqewsZ4EHZhkrmCxOabQR4Eg",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg",
 SUDO_NB: process.env.SUDO_NB || "94768697409",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
